fde368d7904de2bbc2f432236ba6ce91ba24446b
Muhammad Noman <mhmdnoman01@gmail.com>
2025-08-22 03:15:50 +0600

    finished project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,104 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import supabase from "./supabase-client";
+
+type Todo = {
+  id: number;
+  name: string;
+  isCompleted: boolean;
+};
+
 export default function Home() {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const { data, error } = await supabase.from("TodoList").select("*");
+
+    if (error) {
+      console.log("Error fetching: ", error);
+    } else {
+      setTodoList(data as Todo[]);
+    }
+  };
+
+  const addTodo = async () => {
+    const newTodoData = {
+      name: newTodo,
+      isCompleted: false,
+    };
+
+    const { data, error } = await supabase
+      .from("TodoList")
+      .insert([newTodoData])
+      .single();
+
+    if (error) {
+      console.log("Error adding todo", error);
+    } else {
+      setTodoList((prev) => [...prev, data]);
+      setNewTodo("");
+    }
+  };
+
+  const completeTask = async (id, isCompleted) => {
+    const { data, error } = await supabase
+      .from("TodoList")
+      .update({ isCompleted: !isCompleted })
+      .eq("id", id);
+
+    if (error) {
+      console.log("Error toggling taks: ", error);
+    } else {
+      const updatedTodoList = todoList.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !isCompleted } : todo
+      );
+      setTodoList(updatedTodoList);
+    }
+  };
+
+  const deleteTask = async (id) => {
+    const { data, error } = await supabase
+      .from("TodoList")
+      .delete()
+      .eq("id", id);
+
+    if (error) {
+      console.log("Error deleting taks: ", error);
+    } else {
+      setTodoList((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="New Todo..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo Item</button>
+      </div>
+
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            <p>{todo.name}</p>
+            <button onClick={() => completeTask(todo.id, todo.isCompleted)}>
+              {todo.isCompleted ? "Undo" : "Complete Task"}
+            </button>
+            <button onClick={() => deleteTask(todo.id)}>Delete Task</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
